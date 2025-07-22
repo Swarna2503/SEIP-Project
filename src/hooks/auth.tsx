a604ed0c602ae9367bd4b69e0acc8a88919d167b
Shalalalaa <99515332+Shalalalaa@users.noreply.github.com>
2025-07-21 23:38:47 -0500

    fix the login bug after merge, since the login page cannot navigate to the next page

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -39,7 +39,7 @@ export function AuthProvider({ children }: { children: ReactNode }) {
   useEffect(() => {
     setLoading(true);
     getAPIBaseURL().then((baseURL) => {
-      fetch(`${baseURL}/profile`, { credentials: "include" })
+      fetch(`${baseURL}/api/profile`, { credentials: "include" })
         .then(res => {
           if (!res.ok) throw new Error("未登录");
           return res.json();
